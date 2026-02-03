e5b853e41708f2b2266df383b2f3f515e38856bf
Amogh <amoghvp1914@gmail.com>
2026-02-03 13:06:11 +0530

    Add madlibs game

---
+++ madlibsgame.py
@@ -0,0 +1,14 @@
+#madlibs game
+# it is a word game where you create a story based on user input
+verb1 = input("enter a verb ending with 'ing' : ")
+emotion=input("enter an emotion : ")
+adjective1=input("enter an adjective : ")
+verbpast=input("enter a verb in past form like ending with 'ed' : ")
+verb2= input("enter a verb ending with 'ing' : ")
+
+
+print(f"while returning from school i saw my teahcer {verb1}")
+print(f"he was very {emotion} and started telling me how {adjective1} his wife was")
+print(f"but the plot started as he started telling me how he had {verbpast} his wife last night")
+print(f"i started {verb2} in hurry")
+
